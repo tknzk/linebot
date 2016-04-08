e16c274ad028a22a64dc9f1140d93bce4a6490e4
tknzk <tkm.knzk@gmail.com>
2016-04-08 10:26:53 +0900

    logging

--- app.rb
+++ app.rb
@@ -9,7 +9,6 @@ post '/callback' do
   params = JSON.parse(request.body.read)
 
   params['result'].each do |msg|
-    logger.debug(msg.inspect)
     request_content = {
       to: [msg['content']['from']],
       toChannel: 1383378250, # Fixed  value
@@ -20,6 +19,7 @@ post '/callback' do
     http_client = HTTPClient.new
     endpoint_uri = 'https://trialbot-api.line.me/v1/events'
     content_json = request_content.to_json
+    logger.info(content_json)
     http_client.post_content(endpoint_uri, content_json,
                              'Content-Type' => 'application/json; charset=UTF-8',
                              'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
