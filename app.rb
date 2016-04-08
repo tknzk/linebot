2ee8312df2dd67689d2c761e1f419b8b57d2a1b8
tknzk <tkm.knzk@gmail.com>
2016-04-08 10:35:07 +0900

    rest-client

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'json'
-require 'httpclient'
 require 'logger'
+require 'rest-client'
 
 get '/hello' do
   puts 'world'
@@ -17,16 +17,16 @@ post '/callback' do
       content: msg['content']
     }
 
-    http_client = HTTPClient.new
     endpoint_uri = 'https://trialbot-api.line.me/v1/events'
     content_json = request_content.to_json
     logger.info(content_json)
-    http_client.post_content(endpoint_uri, content_json,
-                             'Content-Type' => 'application/json; charset=UTF-8',
-                             'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
-                             'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
-                             'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"]
-                            )
+    RestClient.proxy = ENV["FIXIE_URL"]
+    RestClient.post(endpoint_uri, content_json, {
+      'Content-Type' => 'application/json; charset=UTF-8',
+      'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+      'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+      'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+    })
   end
 
   "OK"
