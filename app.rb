71569397ddef1a4c3ff0d6960855ce305eb1e6f3
tknzk <tkm.knzk@gmail.com>
2016-04-08 10:15:41 +0900

    add app

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'json'
+require 'httpclient'
+
+get 'hello' do
+  puts 'world'
+end
+
+post '/linebot/callback' do
+  params = JSON.parse(request.body.read)
+
+  params['result'].each do |msg|
+    request_content = {
+      to: [msg['content']['from']],
+      toChannel: 1461526144, # Fixed  value
+      eventType: "138311609000106303", # Fixed value
+      content: msg['content']
+    }
+
+    http_client = HTTPClient.new
+    endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+    content_json = request_content.to_json
+    http_client.post_content(endpoint_uri, content_json,
+                             'Content-Type' => 'application/json; charset=UTF-8',
+                             'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+                             'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+                             'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"]
+                            )
+  end
+
+  "OK"
+end
